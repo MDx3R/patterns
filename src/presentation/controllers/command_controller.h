85564d4a09ee3de5f802c8078f50bca31b316df3
MDx3R <nationmdxr@gmail.com>
2025-09-06 17:27:58 +0300

    define and implement contorllers

---
+++ src/presentation/controllers/command_controller.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <memory>
+#include "create_milk_use_case.h"
+#include "create_toy_use_case.h"
+#include "create_tv_use_case.h"
+
+class CommandController
+{
+private:
+    std::shared_ptr<ICreateMilkUseCase> milkUseCase;
+    std::shared_ptr<ICreateToyUseCase> toyUseCase;
+    std::shared_ptr<ICreateTVUseCase> tvUseCase;
+
+public:
+    CommandController(std::shared_ptr<ICreateMilkUseCase> milk,
+                      std::shared_ptr<ICreateToyUseCase> toy,
+                      std::shared_ptr<ICreateTVUseCase> tv)
+        : milkUseCase(std::move(milk)),
+          toyUseCase(std::move(toy)),
+          tvUseCase(std::move(tv)) {}
+
+    int createMilk(const CreateMilkDTO &dto)
+    {
+        return milkUseCase->execute(dto);
+    }
+
+    int createToy(const CreateToyDTO &dto)
+    {
+        return toyUseCase->execute(dto);
+    }
+
+    int createTV(const CreateTVDTO &dto)
+    {
+        return tvUseCase->execute(dto);
+    }
+};
