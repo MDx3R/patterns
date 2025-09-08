837cbd119b82688ca648085db75bc686f458d6e4
MDx3R <nationmdxr@gmail.com>
2025-09-08 12:40:43 +0300

    define and implement movie serive

--- factory/movie.h
+++ factory/movie.h
@@ -9,15 +9,28 @@ class Movie
 {
 private:
     std::unique_ptr<AudioTrack> audio;
-    std::unique_ptr<SubtitlesFile> subs;
+    std::unique_ptr<SubtitlesFile> subtitles;
 
 public:
-    Movie(std::unique_ptr<AudioTrack> audio,
-          std::unique_ptr<SubtitlesFile> subs)
+    Movie(std::unique_ptr<AudioTrack> aud,
+          std::unique_ptr<SubtitlesFile> subs) : audio{std::move(aud)}, subtitles{std::move(subs)}
     {
-        if (audio->language() != subs->language())
+        validateAudioAndSubtitles();
+    };
+
+    void setAudioAndSubtitles(std::unique_ptr<AudioTrack> aud, std::unique_ptr<SubtitlesFile> subs)
+    {
+        this->audio = std::move(aud);
+        this->subtitles = std::move(subs);
+        validateAudioAndSubtitles();
+    }
+
+private:
+    void validateAudioAndSubtitles()
+    {
+        if (audio->language() != subtitles->language())
         {
             throw std::invalid_argument("Language for audio and subtitles must match");
         }
-    };
+    }
 };
